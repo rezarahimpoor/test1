7e149ce2c5e22190fb0230ff5c9619944c6831dc
rezarahimpoor <rezA137872@GMAIL.COM>
2025-04-08 00:05:10 +0330

    full-frount-end

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
-  });
+  const addUserButton =document.getElementById('add_user_button');
+    addUserButton.addEventListener("click" ,()=>{
+      document.querySelector('.add_user_modal').classList.add('show')
+      document.querySelector('.add_user_modal_back').classList.remove('dis-none')
+    })
+    document.querySelector('.add_user_modal_back').addEventListener("click",(e)=>{
+      document.querySelector('.add_user_modal').classList.remove('show')
+      e.target.classList.add('dis-none')
+    })
+    // an oder show for uper javascript
+    // document.querySelector('.add_user_modal_back').addEventListener("click",()=>{
+    // document.querySelector('.add_user_modal').classList.remove('show')
+    //document.querySelector('.add_user_modal_back').classList.add('dis-none')
+    // })
+  })
